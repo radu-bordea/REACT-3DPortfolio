6406084488a9b9601fd5a81947471f35a7f061ca
radu-bordea <radu.bordea.dev@gmail.com>
2024-12-21 21:09:43 +0200

    create hero page

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 // commands
 - npm install 
 - npm run dev
+- npm install styled-components
+
+
+
+// technologies used
+- vite
+- react
+- styled-components
+- 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-function App() {
+import styled from "styled-components"; // Ensure styled-components is imported
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
 
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg4.jpg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
+import React from "react";
+
+const App = () => {
   return (
-    <div>
-      Hello world!
-    </div>
-  )
-}
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
+};
 
-export default App
+export default App;
