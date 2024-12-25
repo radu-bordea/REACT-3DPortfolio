a482f4d66ea7aa086a2e814b7999e2b26c98c5e7
radu-bordea <radu.bordea.dev@gmail.com>
2024-12-25 18:00:54 +0200

    create contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,77 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f1f1f1;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f1f1f1;
+`;
+
+const Button = styled.button`
+  background-color: #ff7518;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 const Contact = () => {
   return (
-    <Section>Contact</Section>
-  )
-}
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <TextArea placeholder="Write your message" rows={10}/>
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
+};
 
-export default Contact
+export default Contact;
